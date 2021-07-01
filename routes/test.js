756768c77175ef787f91e4248a7933672d28723c
bednidhi <bednidhi.rijal2013@gmail.com>
2021-07-02 06:58:24 +1000

    Initial commit

--- routes/test.js
+++ routes/test.js
@@ -5,7 +5,8 @@ let router = express.Router();
 router.get("/hari", (req, res, next) => {
     let objT = {field:{},error:{}}
     let x = objT.field;
-    x['name'] = "hari"
+    x['name'] = "shyam"
+    console.log(x);
   res.send("hello i am from test1"+objT.field.name +"  "+ Object.error);
 });
 
