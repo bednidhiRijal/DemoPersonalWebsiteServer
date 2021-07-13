d785929cb70d9e666011e94c2bff7a180c8915f6
bednidhi <bednidhi.rijal2013@gmail.com>
2021-07-13 12:14:43 +1000

    heroku setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const multer = require('multer')
 var upload = multer();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 //const path = require("path")
 //app.use(express.static(path.join(__dirname,'../public')))
